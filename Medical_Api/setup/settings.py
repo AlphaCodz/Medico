bbc5f091087d736d848aa4a50e6c612728bee6e0
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-03-31 17:02:12 +0100

    CORES

--- Medical_Api/setup/settings.py
+++ Medical_Api/setup/settings.py
@@ -149,3 +149,28 @@ AUTH_USER_MODEL="api.PrimaryUser"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
